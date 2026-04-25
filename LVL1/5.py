3b0531f0fcf21163a0f3903a3603fea82df902ea
Gaurang mane <gaurangmanee09@gmail.com>
2026-04-25 15:35:04 +0530

    Create 5.py

---
+++ LVL1/5.py
@@ -0,0 +1,19 @@
+"""
+* This program is a display of mulitple methods to swap two variables
+* author gaurang mane
+* date 25.04.2026
+"""
+
+#lets take some user input to make it fun and interactive
+a = input("Enter value for 'a': ")
+b = input("Enter value for 'b': ")
+
+#only in python we can swap the variables with method below
+a,b = b,a
+print(f"\nFirst Swap: a = {a} ; b = {b}")
+
+#otherwise we must use a third variable (here temp)
+temp = a
+a = b
+b = temp
+print(f"\nSecond Swap: a = {a} ; b = {b}")
