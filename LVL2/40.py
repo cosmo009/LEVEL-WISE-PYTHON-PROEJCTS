5c50e11d4391fb86ca7182ca5c806ea01a9cf787
Gaurang mane <gaurangmanee09@gmail.com>
2026-05-16 09:06:32 +0530

    Update 40.py

--- LVL2/40.py
+++ LVL2/40.py
@@ -1,5 +1,16 @@
+import numpy as np
+
 print("\n---TO DO LIST (CLI)---\n")
 
-tasks = [[[],[]]]
+tasks = [["1. Sample String task"], ["2. Sample string task"]]
 
-def addTask(task[])
+def addTask(taskList: list,string: str):
+    li = []
+    li.append(string)
+    taskList.append(li)
+    return taskList
+
+tasks = addTask(tasks,"3. Water the shoe")
+print(tasks)
+
+# this isnt done yet. I am going to use numpy's 2D arrays
